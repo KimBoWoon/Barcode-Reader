2a473f91fb7eb16b1f202c7ca032a74a98cb4dce
KimBoWoon <haioer@naver.com>
2019-12-09 15:45:07 +0900

    init

---
+++ app/src/main/java/com/bowoon/android/barcode_reader/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bowoon.android.barcode_reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
